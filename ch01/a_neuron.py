77be2266c29dcdd4a547e296dea18b5edb502017
Ryan Sharif <ryansharif@me.com>
2015-12-17 19:43:19 -0800

    completed initial design of artificial neuron

--- ch01/a_neuron.py
+++ ch01/a_neuron.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python
 
-'''Implementation of a simple artificial neuron in Python.
-   NumPy is required.'''
+import numpy as np
+
+'''Implementation of a simple artificial neuron in Python.'''
 
 class Neuron:
-    pass
+    def __init__(self, weights, bias, function):
+        self.weights = weights
+        self.bias = biad
+        self.function = function
+
+    def forward(self, inputs):
+        logit = np.dot(inputs, self.weights) + self.bias
+        output = self.function(logit)
+        return output
+
+
